21dccc1a8854671e59597505f2eb614f27bb98e9
VegaYed <osvaldoleivabarria1@gmail.com>
2020-05-15 21:36:42 -0400

    modelo asignatura

--- src/main/java/cl/sdm/escuela/modelo/Asignatura.java
+++ src/main/java/cl/sdm/escuela/modelo/Asignatura.java
@@ -1,8 +1,79 @@
 package cl.sdm.escuela.modelo;
 
+import java.util.Objects;
+
 public class Asignatura {
     
     private Long id = null;
     private String nombre;
-    private Long idProfesor = null;
+    private Long idProfesor;
+
+    public Asignatura(String nombre, Long idProfesor) {
+        this.nombre = nombre;
+        this.idProfesor = idProfesor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Long idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "id=" + id + ", nombre=" + nombre + ", idProfesor=" + idProfesor + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.nombre);
+        hash = 29 * hash + Objects.hashCode(this.idProfesor);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Asignatura other = (Asignatura) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idProfesor, other.idProfesor)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
