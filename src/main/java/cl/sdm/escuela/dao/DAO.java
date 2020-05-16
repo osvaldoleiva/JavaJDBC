8fe1240370b7ea21833f45d4124fa13aa3dabd7d
VegaYed <osvaldoleivabarria1@gmail.com>
2020-05-16 03:35:13 -0400

    dao principal

---
+++ src/main/java/cl/sdm/escuela/dao/DAO.java
@@ -0,0 +1,16 @@
+package cl.sdm.escuela.dao;
+
+import java.util.List;
+
+public interface DAO <T, K> {
+    
+    void insertar(T a);
+    
+    void modificar(T a);
+    
+    void eliminar(T a);
+    
+    List<T> obtenerTodos();
+    
+    T obtener(K id);
+}
